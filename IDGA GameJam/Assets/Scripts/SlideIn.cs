fc6804d189a91f0826b747ce062bc91381579862
Eric <EricMouledoux@Gmail.com>
2015-11-15 13:04:29 -0600

    6541

---
+++ IDGA GameJam/Assets/Scripts/SlideIn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideIn : MonoBehaviour
+{
+	
+	void Start () {
+	
+	}
+
+	void Update ()
+	{
+		if(GetComponentInChildren<LifeProgress>().progress.transform.localScale.x >= 1f)
+		{
+			if(transform.localScale.x < 1f)
+			{
+				transform.localScale += new Vector3(Time.deltaTime * 10, 0, 0);
+			}
+			if(transform.localScale.x > 1f)
+			{
+				transform.localScale = new Vector3 (1, 1, 1);
+			}
+		}
+	}
+}
